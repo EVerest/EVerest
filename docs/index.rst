463ced6c3a7060b96e3cc10fe07a6a0b4167cf03
aw <aw@pionix.de>
2022-11-15 15:13:42 +0100

    Added generator script for YAML files
    - this script, as opposed to the previous json version is intended to be
      used such that a corresponding everest-core repository is passed in
      directly for fetching the yamls and docs files
    - updating the pipeline / github action code will come with a following
      PR
    
    Signed-off-by: aw <aw@pionix.de>

--- docs/index.rst
+++ docs/index.rst
@@ -4,11 +4,19 @@
     :align: right
 
 EVerest
-==================
+=======
+
 *Current status of Q1 2022*
 
-The primary goal of EVerest is to develop and maintain an open source software stack for EV charging infrastructure. EVerest is developed having modularity and customizability in mind, so it consists of a framework to configure several interchangeable modules which are coupled by MQTT with each other. EVerest will help to speed the adoption to e-mobility by utilizing all the open source advantages for the EV charging world. It will also enable new features for local energy management, PV-integration, and many more.
-The EVerest project was initiated by PIONIX GmbH, to help with the electrification of the mobility sector.
+The primary goal of EVerest is to develop and maintain an open source software
+stack for EV charging infrastructure. EVerest is developed having modularity
+and customizability in mind, so it consists of a framework to configure several
+interchangeable modules which are coupled by MQTT with each other. EVerest will
+help to speed the adoption to e-mobility by utilizing all the open source
+advantages for the EV charging world. It will also enable new features for
+local energy management, PV-integration, and many more. The EVerest project was
+initiated by PIONIX GmbH, to help with the electrification of the mobility
+sector.
 
 For questions and support please join the `EVerest mailing list <https://lists.lfenergy.org/g/everest>`_.
 There is also a calendar for monthly dev calls.
---
+++ docs/scripts/process_yaml.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env -S python3 -tt
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2020 - 2022 Pionix GmbH and Contributors to EVerest
+#
+
+import argparse
+from typing import List, NamedTuple
+import jinja2
+import yaml
+from pathlib import Path
+from shutil import copytree
+
+
+class YAMLItem(NamedTuple):
+    name: str
+    path: Path
+
+
+YAMLItemList = List[YAMLItem]
+
+# Jinja filters
+
+
+def rst_indent(input):
+    lines = input.splitlines()
+    lines = [f"| { line }\r\n" for line in lines]
+    return "".join(lines)
+
+
+def make_rst_ref(input):
+    output = input.replace("/", "")
+    output = output.replace("#", "-")
+    return output
+
+
+def process_index_file(
+    template: jinja2.Template,
+    file_list: YAMLItemList,
+    entry_ref_prefix: str,
+    refname: str,
+    headline: str,
+    out_path: Path,
+):
+
+    prefix_list = [f"{ entry_ref_prefix }/{ item.name }" for item in file_list]
+    output = template.render(
+        file_list=prefix_list,
+        refname=refname,
+        headline=headline,
+    )
+    out_path.write_text(output)
+
+
+def process_file_list(
+    template: jinja2.Template,
+    file_list: YAMLItemList,
+    out_dir: Path,
+    handwritten_modules: list
+):
+    if not out_dir.is_dir():
+        out_dir.mkdir(parents=True)
+
+    for item in file_list:
+        include_handwritten = False
+        if item.name in handwritten_modules:
+            include_handwritten = True
+        print(f"Process { item.path }")
+        output = template.render(
+            name=item.name,
+            data=yaml.safe_load(item.path.read_text()),
+            include_handwritten=include_handwritten,
+        )
+
+        (out_dir / item.name).with_suffix(".rst").write_text(output)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Process YAML files and jinja2 templates to generate documentation.')
+    parser.add_argument(
+        '--doc-directory',
+        '-d',
+        type=Path,
+        dest='doc_dir',
+        action='store',
+        default=Path(),
+        help="Output directory for generated files"
+    )
+    parser.add_argument(
+        '--core-directory',
+        '-c',
+        type=Path,
+        dest='core_dir',
+        action='store',
+        required=True,
+        help="EVerest core directory"
+    )
+
+    args = parser.parse_args()
+
+    # check for the doc dir and create include and generated folders
+    if not args.doc_dir.is_dir():
+        args.doc_dir.mkdir()
+
+    generated_dir: Path = (args.doc_dir / "generated")
+    included_dir: Path = (args.doc_dir / "included/everest_core")
+    template_dir: Path = (args.doc_dir / "templates")
+
+    # setup templates
+    if not template_dir.is_dir():
+        raise FileNotFoundError(f"Template path: '{ template_dir }' doesn't exist")
+
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(template_dir),
+        trim_blocks=True,
+        lstrip_blocks=True
+    )
+    env.filters['rst_indent'] = rst_indent
+    env.filters['make_rst_ref'] = make_rst_ref
+
+
+
+    # check if everest core directory is valid
+    if not args.core_dir.is_dir():
+        raise FileNotFoundError(f"Everest core path: '{ args.core_dir }' doesn't exist")
+
+    # check for hand written modules
+    handwritten_modules: List[str] = []
+    custom_doc_path: Path = args.core_dir / "docs/modules"
+    if custom_doc_path.is_dir():
+        for item in custom_doc_path.iterdir():
+            if item.suffix != ".rst":
+                continue
+            handwritten_modules.append(item.stem)
+
+        included_dir.mkdir(exist_ok=True, parents=True)
+        copytree(custom_doc_path, included_dir, dirs_exist_ok=True)
+
+    # start generating from YAMLs
+    # modules
+    file_list: YAMLItemList = []
+    for module_path in (args.core_dir / "modules").iterdir():
+        manifest_path = module_path / "manifest.yaml"
+        if not manifest_path.is_file():
+            continue
+        file_list.append(YAMLItem(module_path.stem, manifest_path))
+
+    process_file_list(
+        env.get_template("module.rst.jinja"),
+        file_list,
+        generated_dir / "modules",
+        handwritten_modules
+    )
+    process_index_file(
+        env.get_template("file_list_ref.rst.jinja"),
+        file_list,
+        "modules",
+        "everest_modules",
+        "EVerest Modules",
+        generated_dir / "everest_modules.rst",
+    )
+
+    # interfaces
+    file_list = [YAMLItem(item.stem, item)
+                 for item in (args.core_dir / "interfaces").iterdir() if item.suffix == ".yaml"]
+    process_file_list(
+        env.get_template("interface.rst.jinja"),
+        file_list,
+        generated_dir / "interfaces",
+        handwritten_modules
+    )
+    process_index_file(
+        env.get_template("file_list_ref.rst.jinja"),
+        file_list,
+        "interfaces",
+        "everest_interfaces",
+        "EVerest Interfaces",
+        generated_dir / "everest_interfaces.rst",
+    )
+
+    # types
+    file_list = [YAMLItem(item.stem, item) for item in (args.core_dir / "types").iterdir() if item.suffix == ".yaml"]
+    process_file_list(
+        env.get_template("types.rst.jinja"),
+        file_list,
+        generated_dir / "types",
+        handwritten_modules
+    )
+    process_index_file(
+        env.get_template("file_list_ref.rst.jinja"),
+        file_list,
+        "types",
+        "everest_types",
+        "EVerest Types",
+        generated_dir / "everest_types.rst",
+    )
+
+
+if __name__ == "__main__":
+    main()
