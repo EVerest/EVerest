39ef7f4676f6e255f065e1639274c83f9d1598e1
Dima Dorezyuk <ddo@qwello.eu>
2026-02-17 20:05:41 +0100

    fix formatting
    Signed-off-by: Dima Dorezyuk <ddo@qwello.eu>

--- lib/everest/timer/include/everest/timer.hpp
+++ lib/everest/timer/include/everest/timer.hpp
@@ -126,13 +126,15 @@ public:
                     return;
                 }
 
-                this->timer->expires_after(std::chrono::duration_cast<typename TimerClock::duration>(this->interval_nanoseconds));
+                this->timer->expires_after(
+                    std::chrono::duration_cast<typename TimerClock::duration>(this->interval_nanoseconds));
                 this->timer->async_wait(this->callback_wrapper);
 
                 this->callback();
             };
 
-            this->timer->expires_after(std::chrono::duration_cast<typename TimerClock::duration>(this->interval_nanoseconds));
+            this->timer->expires_after(
+                std::chrono::duration_cast<typename TimerClock::duration>(this->interval_nanoseconds));
             this->timer->async_wait(this->callback_wrapper);
         }
     }
