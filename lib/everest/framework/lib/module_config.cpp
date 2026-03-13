154ea8baebb09c80e2568c1c24c9a44f0dc29ffd
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2026-03-13 16:05:45 +0100

    Refactor get_module_config to also use mqtt.get() with retry
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- lib/everest/framework/lib/module_config.cpp
+++ lib/everest/framework/lib/module_config.cpp
@@ -76,25 +76,6 @@ json get_definitions(std::shared_ptr<MQTTAbstraction> mqtt) {
 json get_module_config(std::shared_ptr<MQTTAbstraction> mqtt, const std::string& module_id) {
     const auto& everest_prefix = mqtt->get_everest_prefix();
 
-    const auto get_config_topic = fmt::format("{}config/request", everest_prefix);
-    const auto response_config_topic = fmt::format("{}modules/{}/response", everest_prefix, module_id);
-    std::promise<json> res_promise;
-    std::future<json> res_future = res_promise.get_future();
-
-    const auto res_handler = [module_id, &res_promise](const std::string& /*topic*/, json json_response) {
-        EVLOG_verbose << fmt::format("Incoming config for {}", module_id);
-        config::Response response = json_response;
-        if (response.status == config::ResponseStatus::Ok and response.type.has_value() and
-            response.type.value() == config::Type::Get) {
-            auto get_response = std::get<config::GetResponse>(response.response);
-            res_promise.set_value(std::move(get_response.data));
-        }
-    };
-
-    const std::shared_ptr<TypedHandler> res_token =
-        std::make_shared<TypedHandler>(HandlerType::GetConfigResponse, std::make_shared<Handler>(res_handler));
-    mqtt->register_handler(response_config_topic, res_token, QOS::QOS2);
-
     config::GetRequest get_request;
     get_request.type = config::GetType::Module;
     config::Request request;
@@ -102,28 +83,20 @@ json get_module_config(std::shared_ptr<MQTTAbstraction> mqtt, const std::string&
     request.origin = module_id;
     request.type = config::Type::Get;
 
-    MqttMessagePayload payload{MqttMessageType::GetConfig, request};
-    mqtt->publish(get_config_topic, payload, QOS::QOS2);
-
-    // wait for result future
-    const std::chrono::time_point<std::chrono::steady_clock> res_wait =
-        std::chrono::steady_clock::now() + std::chrono::milliseconds(mqtt_get_config_timeout_ms);
-    std::future_status res_future_status = std::future_status::deferred;
-    do {
-        res_future_status = res_future.wait_until(res_wait);
-    } while (res_future_status == std::future_status::deferred);
+    MQTTRequest mqtt_request;
+    mqtt_request.response_topic = fmt::format("{}modules/{}/response", everest_prefix, module_id);
+    mqtt_request.request_topic = fmt::format("{}config/request", everest_prefix);
+    mqtt_request.request_data = json(request).dump();
 
     json result;
-    if (res_future_status == std::future_status::timeout) {
-        mqtt->unregister_handler(response_config_topic, res_token);
-        EVLOG_AND_THROW(
-            EverestTimeoutError(fmt::format("Timeout while waiting for result of get_config of {}", module_id)));
-    }
-    if (res_future_status == std::future_status::ready) {
-        result = res_future.get();
-    }
-    mqtt->unregister_handler(response_config_topic, res_token);
 
+    config::Response response = mqtt->get(mqtt_request, mqtt_get_config_retries);
+    EVLOG_verbose << fmt::format("Incoming config for {}", module_id);
+    if (response.status == config::ResponseStatus::Ok and response.type.has_value() and
+        response.type.value() == config::Type::Get) {
+        auto get_response = std::get<config::GetResponse>(response.response);
+        result = get_response.data;
+    }
     result.update(get_definitions(mqtt));
 
     return result;
