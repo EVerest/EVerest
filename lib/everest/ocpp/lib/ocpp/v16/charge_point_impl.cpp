3507b46c1e81e7a40d3705c29e17ee9c75c8e08e
Piet Gömpel <37657534+Pietfried@users.noreply.github.com>
2026-03-13 15:01:40 +0100

    fix(ocpp16): DataTransfer.req(GetInstalledCertificateIds) was incorrectly reporting not found if certificateType was omitted. The specification requires to report all installed certificates in this case (#1951)
    Signed-off-by: Piet Gömpel <pietgoempel@gmail.com>

--- lib/everest/ocpp/lib/ocpp/v16/charge_point_impl.cpp
+++ lib/everest/ocpp/lib/ocpp/v16/charge_point_impl.cpp
@@ -4136,6 +4136,11 @@ void ChargePointImpl::handle_data_transfer_pnc_get_installed_certificates(Call<D
             std::vector<ocpp::CertificateType> certificate_types;
             if (req.certificateType.has_value()) {
                 certificate_types = ocpp::evse_security_conversions::from_ocpp_v2(req.certificateType.value());
+            } else {
+                // When omitted, all certificate types are requested
+                certificate_types.push_back(CertificateType::V2GRootCertificate);
+                certificate_types.push_back(CertificateType::MORootCertificate);
+                certificate_types.push_back(CertificateType::V2GCertificateChain);
             }
 
             ocpp::v2::GetInstalledCertificateIdsResponse get_certificate_ids_response;
