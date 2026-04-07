430475f26e7e44b221429e04895103d60805aca5
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2026-04-07 23:10:04 +0200

    clang-format
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- lib/everest/framework/include/framework/ModuleAdapter.hpp
+++ lib/everest/framework/include/framework/ModuleAdapter.hpp
@@ -73,7 +73,8 @@ protected:
     }
 
     void shutdown() {
-        EVLOG_warning << "No shutdown handler installed! Please implement shutdown() in your module! (" << info.name << ")";
+        EVLOG_warning << "No shutdown handler installed! Please implement shutdown() in your module! (" << info.name
+                      << ")";
         exit(EXIT_SUCCESS);
     }
 };
