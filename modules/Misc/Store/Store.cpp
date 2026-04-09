688dde0be4a41fde86be34e63f1f9a9cdf714566
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2026-04-09 21:43:52 +0200

    Implement simple shutdown handlers for modules used in example config
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- modules/Misc/Store/Store.cpp
+++ modules/Misc/Store/Store.cpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2020 - 2021 Pionix GmbH and Contributors to EVerest
+// Copyright 2020 - 2026 Pionix GmbH and Contributors to EVerest
 #include "Store.hpp"
 
 namespace module {
@@ -10,6 +10,10 @@ void Store::init() {
 
 void Store::ready() {
     invoke_ready(*p_main);
+}
+
+void Store::shutdown() {
+    invoke_shutdown(*p_main);
 }
 
 } // namespace module
