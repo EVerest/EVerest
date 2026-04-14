6c9ac5f081ff6f9acf5660225256e71b0010adcd
Piet Gömpel <pietgoempel@gmail.com>
2026-04-14 19:29:35 +0200

    feat(framework): add load_yaml_from_string helper
    Signed-off-by: Piet Gömpel <pietgoempel@gmail.com>

--- lib/everest/yaml/include/everest/utils/yaml_loader.hpp
+++ lib/everest/yaml/include/everest/utils/yaml_loader.hpp
@@ -12,6 +12,9 @@ namespace Everest {
 /// \brief Read the YAML file at \p path and returns its content as JSON
 nlohmann::ordered_json load_yaml(const std::filesystem::path& path);
 
+/// \brief Parse a YAML string and returns its content as JSON
+nlohmann::ordered_json load_yaml_from_string(const std::string& yaml_content);
+
 /// \brief Saves the JSON \p data to a YAML file at \p path
 void save_yaml(const nlohmann::ordered_json& data, const std::filesystem::path& path);
 
