c456a527885e3393bb3f57a4161e5341ddefd23c
Christoph Burandt <christoph.burandt@pionix.de>
2026-05-06 21:18:38 +0200

    Add entrypoint AsyncAPI specification and extend API types lib
    Signed-off-by: Christoph Burandt <christoph.burandt@pionix.de>

---
+++ lib/everest/everest_api_types/include/everest_api_types/entrypoint/API.hpp
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 - 2025 Pionix GmbH and Contributors to EVerest
+
+#pragma once
+#include <optional>
+#include <string>
+#include <vector>
+
+namespace everest::lib::API::V1_0::types::entrypoint {
+
+struct CommunicationParameters {
+    std::optional<int32_t> heartbeat_period_ms;
+    std::optional<int32_t> communication_check_period_s;
+    std::optional<int32_t> request_reply_timeout_s;
+};
+
+struct ApiParameter {
+    std::string type;
+    std::string module_id;
+    int32_t version;
+    std::optional<CommunicationParameters> communication_monitoring;
+};
+
+struct ApiDiscoverResponse {
+    std::vector<ApiParameter> apis;
+};
+
+} // namespace everest::lib::API::V1_0::types::entrypoint
---
+++ lib/everest/everest_api_types/include/everest_api_types/entrypoint/codec.hpp
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 - 2025 Pionix GmbH and Contributors to EVerest
+
+#pragma once
+#include <everest_api_types/entrypoint/API.hpp>
+#include <optional>
+
+namespace everest::lib::API::V1_0::types::entrypoint {
+
+std::string serialize(ApiParameter val) noexcept;
+std::string serialize(ApiDiscoverResponse val) noexcept;
+std::string serialize(CommunicationParameters val) noexcept;
+
+std::ostream& operator<<(std::ostream& os, ApiParameter const& val);
+std::ostream& operator<<(std::ostream& os, ApiDiscoverResponse const& val);
+std::ostream& operator<<(std::ostream& os, CommunicationParameters const& val);
+
+template <class T> T deserialize(std::string const& val);
+template <class T> std::optional<T> try_deserialize(std::string const& val) {
+    try {
+        return deserialize<T>(val);
+    } catch (...) {
+        return std::nullopt;
+    }
+}
+template <class T> bool adl_deserialize(std::string const& json_data, T& obj) {
+    auto opt = try_deserialize<T>(json_data);
+    if (opt) {
+        obj = opt.value();
+        return true;
+    }
+    return false;
+}
+
+} // namespace everest::lib::API::V1_0::types::entrypoint
