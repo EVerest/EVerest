4ab62a561b6b36b68a8648b6e1933b932b786a92
Christoph Burandt <christoph.burandt@pionix.de>
2026-05-12 11:21:05 +0200

    Add internal management_API OBJECT lib to manager build
    Signed-off-by: Christoph Burandt <christoph.burandt@pionix.de>

---
+++ lib/everest/framework/src/management_api/include/lifecycle_api.hpp
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 - 2026 Pionix GmbH and Contributors to EVerest
+
+/// \file Class to implement the LifecycleAPI, which provides an API to manage the lifecycle of EVerest modules
+
+#pragma once
+
+#include <everest_api_types/utilities/Topics.hpp>
+#include <utils/config_service_interface.hpp>
+#include <utils/mqtt_abstraction.hpp>
+
+namespace ev_API = everest::lib::API;
+
+namespace Everest::api::lifecycle {
+
+enum class ConfigurationApiStatus {
+    NotAvailable,
+    AvailableRO,
+    AvailableRW,
+};
+
+class LifecycleAPI {
+public:
+    LifecycleAPI(MQTTAbstraction& mqtt_abstraction, ::Everest::config::ConfigServiceInterface& config_service,
+                 ConfigurationApiStatus config_api_availability, bool readonly = true);
+
+private:
+    MQTTAbstraction& m_mqtt_abstraction;
+    ::Everest::config::ConfigServiceInterface& m_config_service;
+
+    ev_API::Topics m_topics;
+    ConfigurationApiStatus m_config_api_availability;
+    const bool m_readonly;
+
+    void generate_api_cmd_stop_modules();
+    void generate_api_cmd_start_modules();
+
+    void generate_api_var_status();
+
+    using ParseAndPublishFtor = std::function<bool(std::string const&)>;
+    void subscribe_api_topic(std::string const& var, ParseAndPublishFtor const& parse_and_publish);
+};
+
+} // namespace Everest::api::lifecycle
