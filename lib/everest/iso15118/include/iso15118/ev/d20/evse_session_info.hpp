9f2aa6dc074a4877ce63f1d367285a98d5465b07
Martin Litre <mnlitre@gmail.com>
2026-05-13 09:12:19 +0200

    feat(iso15118): EV D20 session states
    Signed-off-by: Martin Litre <mnlitre@gmail.com>

--- lib/everest/iso15118/include/iso15118/ev/d20/evse_session_info.hpp
+++ lib/everest/iso15118/include/iso15118/ev/d20/evse_session_info.hpp
@@ -4,13 +4,15 @@
 
 #include <iso15118/message/common_types.hpp>
 
+#include <everest/util/vector/fixed_vector.hpp>
+
 namespace iso15118::ev::d20 {
 
 // Holds information reported by the EVSE that could be different between sessions
 struct EVSESessionInfo {
-    std::vector<message_20::datatypes::Authorization> auth_services{};
+    everest::lib::util::fixed_vector<message_20::datatypes::Authorization, 2> auth_services{};
     bool certificate_installation_service{false};
-    std::optional<std::vector<message_20::datatypes::Name>> supported_providers{std::nullopt};
+    std::optional<everest::lib::util::fixed_vector<message_20::datatypes::Name, 128>> supported_providers{std::nullopt};
     message_20::datatypes::GenChallenge gen_challenge{std::array<uint8_t, 16>{}}; // 16 bytes
 };
 
