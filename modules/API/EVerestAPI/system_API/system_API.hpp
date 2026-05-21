4d46af38aa9c5c3d57ca916da693c3c4a0cd07b5
Christoph Burandt <christoph.burandt@pionix.de>
2026-05-21 10:36:01 +0200

    Apply suggestions from code review Copyright lines, batch 2
    Co-authored-by: Kai Hermann <kai.uwe.hermann@gmail.com>
    Signed-off-by: Christoph Burandt <christoph.burandt@pionix.de>

--- modules/API/EVerestAPI/system_API/system_API.hpp
+++ modules/API/EVerestAPI/system_API/system_API.hpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2020 - 2025 Pionix GmbH and Contributors to EVerest
+// Copyright 2020 - 2026 Pionix GmbH and Contributors to EVerest
 #ifndef SYSTEM_API_HPP
 #define SYSTEM_API_HPP
 
