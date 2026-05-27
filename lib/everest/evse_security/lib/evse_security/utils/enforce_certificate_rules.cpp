a103c1d86e8a015a0917e3a86b266a977d96e571
xander <xandergot@gmail.com>
2026-05-27 11:22:33 -0400

    added Copyright
    Signed-off-by: xander <xandergot@gmail.com>

--- lib/everest/evse_security/lib/evse_security/utils/enforce_certificate_rules.cpp
+++ lib/everest/evse_security/lib/evse_security/utils/enforce_certificate_rules.cpp
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright Contributors to the EVerest Project.
+
 #include "evse_security/utils/enforce_certificate_rules.hpp"
 #include <algorithm>
 #include <everest/logging.hpp>
