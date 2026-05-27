1bed111360b67ad1a27c1f6c450ed76227281dbf
xander <xandergot@gmail.com>
2026-05-27 11:24:45 -0400

    added copyright
    Signed-off-by: xander <xandergot@gmail.com>

--- lib/everest/evse_security/lib/evse_security/utils/load_ctl.cpp
+++ lib/everest/evse_security/lib/evse_security/utils/load_ctl.cpp
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright Contributors to the EVerest Project.
+
 #include <algorithm>
 #include <everest/logging.hpp>
 #include <evse_security/evse_types.hpp>
