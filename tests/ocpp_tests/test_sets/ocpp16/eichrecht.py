99432fc3e9cfdd257f57920f9bf937f8efaba6c3
Wire Jansen <mail@nzbr.de>
2026-06-01 19:42:52 +0200

    Also add a similar test for OCCP 2.0.1

--- tests/ocpp_tests/test_sets/ocpp16/eichrecht.py
+++ tests/ocpp_tests/test_sets/ocpp16/eichrecht.py
@@ -18,20 +18,8 @@ from typing import Dict
 
 from everest.testing.core_utils._configuration.everest_configuration_strategies.everest_configuration_strategy import \
     EverestConfigAdjustmentStrategy
-
-
-class YetiSimulatorDisableMeterTransactionStartStrategy(EverestConfigAdjustmentStrategy):
-    def adjust_everest_configuration(self, everest_config: Dict) -> Dict:
-
-        adjusted_config = deepcopy(everest_config)
-
-        for module_id, module_config in adjusted_config.get("active_modules", {}).items():
-            if module_config.get("module") == "YetiSimulator":
-                if "config_module" not in module_config:
-                    module_config["config_module"] = {}
-                module_config["config_module"]["dummy_meter_value_send_on_transaction_start"] = False
-
-        return adjusted_config
+from everest.testing.core_utils._configuration.everest_configuration_strategies.yeti_simulator_disable_meter_transaction_start_strategy import \
+    YetiSimulatorDisableMeterTransactionStartStrategy
 
 
 @pytest.mark.asyncio
