604099be9a533e4c0c9d42775fb7fd6bdac3154a
Martin Lukas <martin.lukas@chargebyte.com>
2026-06-10 11:11:14 +0200

    fix(iso15118): on failed cable check, set ResponseCode to FAILED in CableCheck
    Also set EVSEProcessing to Finished.
    
    ISO 15118-20 is not clear about this, and leaves setting ResponseCode FAILED
    up to interpretation.
    
    IEC 61851-23, targeted at ISO 15118-2, is more clear:
        If the EVSE detects an error, the EVSE shall trigger an EVSE initiated
        error shutdown by sending [...] "ResponseCode" = 'FAILED' in message
        CableCheckRes.
    
    IEC 61851-23-3, targeted more appropriately at ISO 15118-20 (for MCS), states
    this as:
        After completing the cable-check phase, the EVSE shall send message
        DC_CableCheckRes with parameters "EVSEProcessing" = 'Finished', and:
        - "ResponseCode" = ‘OK’, if there are no errors, or
        - "ResponseCode" = 'FAILED', if the EVSE detected an error, to trigger
          an EVSE initiated error shutdown.
    
    Signed-off-by: Martin Lukas <martin.lukas@chargebyte.com>

--- lib/everest/iso15118/include/iso15118/detail/d20/state/dc_cable_check.hpp
+++ lib/everest/iso15118/include/iso15118/detail/d20/state/dc_cable_check.hpp
@@ -2,12 +2,15 @@
 // Copyright 2023 Pionix GmbH and Contributors to EVerest
 #pragma once
 
+#include <optional>
+
 #include <iso15118/d20/session.hpp>
 #include <iso15118/message/dc_cable_check.hpp>
 
 namespace iso15118::d20::state {
 
 message_20::DC_CableCheckResponse handle_request(const message_20::DC_CableCheckRequest& req,
-                                                 const d20::Session& session, bool cable_check_done);
+                                                 const d20::Session& session,
+                                                 const std::optional<bool>& cable_check_success);
 
 } // namespace iso15118::d20::state
