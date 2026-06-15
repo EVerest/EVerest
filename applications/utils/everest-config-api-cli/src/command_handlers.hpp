EVerest/everest-core

Add a CLI configuration_API client
Signed-off-by: Christoph Burandt <christoph.burandt@pionix.de>

applications/utils/everest-config-api-cli/src/command_handlers.hpp
@@ -1,0 +1,30 @@
+#pragma once
+
+#include "i_config_service_client.hpp"
+#include "i_yaml_provider.hpp"
+#include <memory>
+#include <string>
+
+namespace everest::config_cli {
+
+class CommandHandlers {
+public:
+    CommandHandlers(std::shared_ptr<IConfigServiceClient> client, std::shared_ptr<IYamlProvider> yaml_provider);
+
+    void list_slots();
+    void show_slot_metadata(int slot_id);
+    void active_slot();
+    void mark_active_slot(int slot_id);
+    void delete_slot(int slot_id);
+    void duplicate_slot(int slot_id, const std::string& description);
+    void load_yaml(const std::string& filename, const std::string& description, std::optional<int> slot_id);
+    void get_configuration(int slot_id);
+    void set_config_parameter(int slot_id, const std::string& filename);
+    void monitor(bool suppress_parameter_updates);
+
+private:
+    std::shared_ptr<IConfigServiceClient> client_;
+    std::shared_ptr<IYamlProvider> yaml_provider_;
+};
+
+} // namespace everest::config_cli

applications/utils/everest-config-api-cli/src/main.cpp
@@ -1,0 +1,160 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 - 2026 Pionix GmbH and Contributors to EVerest
+
+#include <boost/program_options.hpp>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "command_handlers.hpp"
+#include "everest_api_types/utilities/Topics.hpp"
+#include "mqtt_config_service_client.hpp"
+#include "yaml_provider.hpp"
+
+namespace po = boost::program_options;
+using namespace everest::config_cli;
+
+int main(int argc, char** argv) {
+    std::string host;
+    int port;
+    std::string api_name;
+
+    po::options_description global("Global options");
+    global.add_options()("help,h", "This help message")(
+        "host", po::value<std::string>(&host)->default_value("localhost"),
+        "MQTT broker host")("port", po::value<int>(&port)->default_value(1883), "MQTT broker port")(
+        "api-name", po::value<std::string>(&api_name)->default_value("config_service"),
+        "API name to form the MQTT topic (e.g., config_service)")(
+        "command", po::value<std::string>(), "Command to execute")("cmd_args", po::value<std::vector<std::string>>(),
+                                                                   "Arguments for command");
+
+    po::positional_options_description pos;
+    pos.add("command", 1).add("cmd_args", -1);
+
+    po::variables_map vm;
+    try {
+        po::parsed_options parsed =
+            po::command_line_parser(argc, argv).options(global).positional(pos).allow_unregistered().run();
+        po::store(parsed, vm);
+        po::notify(vm);
+    } catch (const po::error& e) {
+        std::cerr << "Error: " << e.what() << "\n";
+        return 1;
+    }
+
+    if (vm.count("help") || !vm.count("command")) {
+        std::cout << "Usage: everest-config-api-cli [options] <command> [args...]\n\n";
+        std::cout << global << "\n";
+        std::cout << "Commands:\n"
+                  << "  list_slots\n"
+                  << "  show_slot_metadata <slot_id>\n"
+                  << "  active_slot\n"
+                  << "  mark_active_slot <slot_id>\n"
+                  << "  delete_slot <no>\n"
+                  << "  duplicate_slot <no> [<description>]\n"
+                  << "  load_yaml <filename>[--description <desc>] [--slot-id <slot_id>]\n"
+                  << "  get_configuration <slot_id>\n"
+                  << "  set_config_parameter <slot_id> <filename>\n"
+                  << "  monitor [--suppress-parameter-updates]\n";
+        return 0;
+    }
+
+    std::string command = vm["command"].as<std::string>();
+    std::vector<std::string> cmd_args = po::collect_unrecognized(
+        po::command_line_parser(argc, argv).options(global).positional(pos).allow_unregistered().run().options,
+        po::include_positional);
+    // Remove the command itself from cmd_args
+    if (!cmd_args.empty()) {
+        cmd_args.erase(cmd_args.begin());
+    }
+
+    everest::lib::API::Topics topics;
+    topics.setup("_", "config_service", 0);
+
+    try {
+        auto client = std::make_shared<MqttConfigServiceClient>(host, port, topics);
+        auto yaml_provider = std::make_shared<YamlProvider>();
+        CommandHandlers handlers(client, yaml_provider);
+
+        if (command == "list_slots") {
+            handlers.list_slots();
+        } else if (command == "show_slot_metadata") {
+            if (cmd_args.empty()) {
+                std::cerr << "Usage: show_slot_metadata <slot_id>\n";
+                return 1;
+            }
+            handlers.show_slot_metadata(std::stoi(cmd_args[0]));
+        } else if (command == "active_slot") {
+            handlers.active_slot();
+        } else if (command == "mark_active_slot") {
+            if (cmd_args.empty()) {
+                std::cerr << "Usage: mark_active_slot <slot_id>\n";
+                return 1;
+            }
+            handlers.mark_active_slot(std::stoi(cmd_args[0]));
+        } else if (command == "delete_slot") {
+            if (cmd_args.empty()) {
+                std::cerr << "Usage: delete_slot <no>\n";
+                return 1;
+            }
+            handlers.delete_slot(std::stoi(cmd_args[0]));
+        } else if (command == "duplicate_slot") {
+            if (cmd_args.empty()) {
+                std::cerr << "Usage: duplicate_slot <no> [<description>]\n";
+                return 1;
+            }
+            int slot_id = std::stoi(cmd_args[0]);
+            std::string desc = (cmd_args.size() > 1) ? cmd_args[1] : "";
+            handlers.duplicate_slot(slot_id, desc);
+        } else if (command == "load_yaml") {
+            std::string filename;
+            int slot_id = -1;
+            std::string desc;
+            po::options_description load_yaml_opts("load_yaml options");
+            load_yaml_opts.add_options()("filename", po::value<std::string>(&filename), "YAML file to load")(
+                "slot-id", po::value<int>(&slot_id)->notifier([](int v) {
+                    if (v < 0)
+                        throw po::validation_error(po::validation_error::invalid_option_value, "slot-id");
+                }),
+                "Existing slot ID to load into (if not provided, a new slot will be created)")(
+                "description,d", po::value<std::string>(&desc), "Description for the slot");
+            po::positional_options_description load_yaml_pos;
+            load_yaml_pos.add("filename", 1);
+            po::variables_map lvm;
+            po::store(po::command_line_parser(cmd_args).options(load_yaml_opts).positional(load_yaml_pos).run(), lvm);
+            po::notify(lvm);
+            if (filename.empty()) {
+                std::cerr << "Usage: load_yaml <filename> [--description <desc>] [--slot-id <slot_id>]\n";
+                return 1;
+            }
+            handlers.load_yaml(filename, desc, slot_id == -1 ? std::nullopt : std::optional<int>(slot_id));
+        } else if (command == "get_configuration") {
+            if (cmd_args.empty()) {
+                std::cerr << "Usage: get_configuration <slot_id>\n";
+                return 1;
+            }
+            handlers.get_configuration(std::stoi(cmd_args[0]));
+        } else if (command == "set_config_parameter") {
+            if (cmd_args.size() < 2) {
+                std::cerr << "Usage: set_config_parameter <slot_id> <filename>\n";
+                return 1;
+            }
+            handlers.set_config_parameter(std::stoi(cmd_args[0]), cmd_args[1]);
+        } else if (command == "monitor") {
+            bool suppress = false;
+            if (!cmd_args.empty() && cmd_args[0] == "--suppress-parameter-updates") {
+                suppress = true;
+            }
+            handlers.monitor(suppress);
+        } else {
+            std::cerr << "Unknown command: " << command << "\n";
+            return 1;
+        }
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << "\n";
+        return 1;
+    }
+
+    return 0;
+}

applications/utils/everest-config-api-cli/src/mqtt_config_service_client.hpp
@@ -1,0 +1,66 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 - 2026 Pionix GmbH and Contributors to EVerest
+#pragma once
+
+#include "i_config_service_client.hpp"
+
+#include "everest_api_types/utilities/Topics.hpp"
+#include <everest/io/event/event_fd.hpp>
+#include <everest/io/event/fd_event_handler.hpp>
+#include <everest/io/mqtt/mqtt_client.hpp>
+
+#include <atomic>
+#include <cstdint>
+#include <future>
+#include <memory>
+#include <string>
+#include <thread>
+
+namespace everest::config_cli {
+
+class MqttConfigServiceClient : public IConfigServiceClient {
+public:
+    MqttConfigServiceClient(const std::string& host, std::uint16_t port, const everest::lib::API::Topics& topics,
+                            std::uint32_t reconnect_ms = 5000, bool verbose = false);
+    ~MqttConfigServiceClient() override;
+
+    std::optional<ListSlotIdsResult> list_all_slots() override;
+    std::optional<GetActiveSlotIdResult> get_active_slot() override;
+    std::optional<MarkActiveSlotResult> mark_active_slot(int slot_id) override;
+    std::optional<DeleteSlotResult> delete_slot(int slot_id) override;
+    std::optional<DuplicateSlotResult> duplicate_slot(int slot_id, const std::string& description) override;
+    std::optional<LoadFromYamlResult> load_from_yaml(const std::string& raw_yaml, const std::string& description,
+                                                     std::optional<int> slot_id) override;
+    std::optional<GetConfigurationResult> get_configuration(int slot_id) override;
+    std::optional<ConfigurationParameterUpdateRequestResult>
+    set_config_parameters(const ConfigurationParameterUpdateRequest& request) override;
+    void subscribe_to_updates(bool suppress_parameter_updates, ActiveSlotCallback active_cb,
+                              ConfigUpdateCallback config_cb) override;
+
+private:
+    std::optional<std::string> perform_rpc_raw(const std::string& operation, const std::string& payload_json);
+
+    template <typename ReqType, typename ResType>
+    std::optional<ResType> perform_rpc(const std::string& operation, const ReqType& req);
+
+    void setup_update_subscriptions();
+
+    everest::lib::io::event::fd_event_handler event_handler_;
+    everest::lib::io::mqtt::mqtt_client client_;
+
+    everest::lib::API::Topics topics;
+    std::string reply_topic_base_;
+
+    std::atomic<bool> running_{true};
+    everest::lib::io::event::event_fd cancel_event_;
+    std::thread event_loop_thread_;
+
+    std::promise<void> connected_promise_;
+    std::shared_future<void> connected_future_{connected_promise_.get_future().share()};
+
+    bool suppress_parameter_updates_{false};
+    ActiveSlotCallback active_cb_;
+    ConfigUpdateCallback config_cb_;
+};
+
+} // namespace everest::config_cli

EVerest/EVerest

applications/utils/everest-config-api-cli/src/command_handlers.hpp
@@ -1,0 +1,32 @@
+#pragma once
+
+#include "config_service_client_ifc.hpp"
+#include "yaml_provider_ifc.hpp"
+#include <memory>
+#include <string>
+
+namespace everest::config_cli {
+
+class CommandHandlers {
+public:
+    CommandHandlers(std::shared_ptr<ConfigServiceClientIfc> client, std::shared_ptr<YamlProviderIfc> yaml_provider);
+
+    void list_slots();
+    void show_slot_metadata(int slot_id);
+    void get_active_slot();
+    void mark_active_slot(int slot_id);
+    void delete_slot(int slot_id);
+    void duplicate_slot(int slot_id, const std::string& description);
+    void load_yaml(const std::string& filename, const std::string& description, std::optional<int> slot_id);
+    void set_description(int slot_id, const std::string& description);
+    void get_configuration(int slot_id);
+    void set_config_parameters(int slot_id, const std::string& filename);
+    void get_config_parameters(int slot_id, const std::string& filename);
+    void monitor(bool suppress_parameter_updates);
+
+private:
+    std::shared_ptr<ConfigServiceClientIfc> m_client;
+    std::shared_ptr<YamlProviderIfc> m_yaml_provider;
+};
+
+} // namespace everest::config_cli

applications/utils/everest-config-api-cli/src/main.cpp
@@ -1,0 +1,174 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 - 2026 Pionix GmbH and Contributors to EVerest
+
+#include <boost/program_options.hpp>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "command_handlers.hpp"
+#include "everest_api_types/utilities/Topics.hpp"
+#include "mqtt_config_service_client.hpp"
+#include "yaml_provider.hpp"
+
+namespace po = boost::program_options;
+using namespace everest::config_cli;
+
+int main(int argc, char** argv) {
+    std::string host;
+    int port;
+    std::string api_name;
+
+    po::options_description global("Global options");
+    global.add_options()("help,h", "This help message")(
+        "host", po::value<std::string>(&host)->default_value("localhost"),
+        "MQTT broker host")("port", po::value<int>(&port)->default_value(1883), "MQTT broker port")(
+        "api-name", po::value<std::string>(&api_name)->default_value("configuration"),
+        "API name to form the MQTT topic (e.g., configuration)")(
+        "command", po::value<std::string>(), "Command to execute")("cmd_args", po::value<std::vector<std::string>>(),
+                                                                   "Arguments for command");
+
+    po::positional_options_description pos;
+    pos.add("command", 1).add("cmd_args", -1);
+
+    po::variables_map vm;
+    try {
+        po::parsed_options parsed =
+            po::command_line_parser(argc, argv).options(global).positional(pos).allow_unregistered().run();
+        po::store(parsed, vm);
+        po::notify(vm);
+    } catch (const po::error& e) {
+        std::cerr << "Error: " << e.what() << "\n";
+        return 1;
+    }
+
+    if (vm.count("help") || !vm.count("command")) {
+        std::cout << "Usage: everest-config-api-cli [options] <command> [args...]\n\n";
+        std::cout << global << "\n";
+        std::cout << "Commands:\n"
+                  << "  list_slots\n"
+                  << "  show_slot_metadata <slot_id>\n"
+                  << "  get_active_slot\n"
+                  << "  mark_active_slot <slot_id>\n"
+                  << "  delete_slot <no>\n"
+                  << "  duplicate_slot <no> [<description>]\n"
+                  << "  load_yaml <filename>[--description <desc>] [--slot-id <slot_id>]\n"
+                  << "  set_description <slot_id> <description>\n"
+                  << "  get_configuration <slot_id>\n"
+                  << "  set_config_parameters <slot_id> <filename>\n"
+                  << "  get_config_parameters <slot_id> <filename>\n"
+                  << "  monitor [--suppress-parameter-updates]\n";
+        return 0;
+    }
+
+    std::string command = vm["command"].as<std::string>();
+    std::vector<std::string> cmd_args = po::collect_unrecognized(
+        po::command_line_parser(argc, argv).options(global).positional(pos).allow_unregistered().run().options,
+        po::include_positional);
+    // Remove the command itself from cmd_args
+    if (!cmd_args.empty()) {
+        cmd_args.erase(cmd_args.begin());
+    }
+
+    everest::lib::API::Topics topics;
+    topics.setup("_", "configuration", 0);
+
+    try {
+        auto client = std::make_shared<MqttConfigServiceClient>(host, port, topics);
+        auto yaml_provider = std::make_shared<YamlProvider>();
+        CommandHandlers handlers(client, yaml_provider);
+
+        if (command == "list_slots") {
+            handlers.list_slots();
+        } else if (command == "show_slot_metadata") {
+            if (cmd_args.empty()) {
+                std::cerr << "Usage: show_slot_metadata <slot_id>\n";
+                return 1;
+            }
+            handlers.show_slot_metadata(std::stoi(cmd_args[0]));
+        } else if (command == "get_active_slot") {
+            handlers.get_active_slot();
+        } else if (command == "mark_active_slot") {
+            if (cmd_args.empty()) {
+                std::cerr << "Usage: mark_active_slot <slot_id>\n";
+                return 1;
+            }
+            handlers.mark_active_slot(std::stoi(cmd_args[0]));
+        } else if (command == "delete_slot") {
+            if (cmd_args.empty()) {
+                std::cerr << "Usage: delete_slot <no>\n";
+                return 1;
+            }
+            handlers.delete_slot(std::stoi(cmd_args[0]));
+        } else if (command == "duplicate_slot") {
+            if (cmd_args.empty()) {
+                std::cerr << "Usage: duplicate_slot <no> [<description>]\n";
+                return 1;
+            }
+            int slot_id = std::stoi(cmd_args[0]);
+            std::string desc = (cmd_args.size() > 1) ? cmd_args[1] : "";
+            handlers.duplicate_slot(slot_id, desc);
+        } else if (command == "load_yaml") {
+            std::string filename;
+            int slot_id = -1;
+            std::string desc;
+            po::options_description load_yaml_opts("load_yaml options");
+            load_yaml_opts.add_options()("filename", po::value<std::string>(&filename), "YAML file to load")(
+                "slot-id", po::value<int>(&slot_id)->notifier([](int v) {
+                    if (v < 0)
+                        throw po::validation_error(po::validation_error::invalid_option_value, "slot-id");
+                }),
+                "Existing slot ID to load into (if not provided, a new slot will be created)")(
+                "description,d", po::value<std::string>(&desc), "Description for the slot");
+            po::positional_options_description load_yaml_pos;
+            load_yaml_pos.add("filename", 1);
+            po::variables_map lvm;
+            po::store(po::command_line_parser(cmd_args).options(load_yaml_opts).positional(load_yaml_pos).run(), lvm);
+            po::notify(lvm);
+            if (filename.empty()) {
+                std::cerr << "Usage: load_yaml <filename> [--description <desc>] [--slot-id <slot_id>]\n";
+                return 1;
+            }
+            handlers.load_yaml(filename, desc, slot_id == -1 ? std::nullopt : std::optional<int>(slot_id));
+        } else if (command == "set_description") {
+            if (cmd_args.empty()) {
+                std::cerr << "Usage: set_description <slot_id> <description>\n";
+                return 1;
+            }
+            handlers.set_description(std::stoi(cmd_args[0]), cmd_args[1]);
+        } else if (command == "get_configuration") {
+            if (cmd_args.empty()) {
+                std::cerr << "Usage: get_configuration <slot_id>\n";
+                return 1;
+            }
+            handlers.get_configuration(std::stoi(cmd_args[0]));
+        } else if (command == "set_config_parameters") {
+            if (cmd_args.size() < 2) {
+                std::cerr << "Usage: set_config_parameters <slot_id> <filename>\n";
+                return 1;
+            }
+            handlers.set_config_parameters(std::stoi(cmd_args[0]), cmd_args[1]);
+        } else if (command == "get_config_parameters") {
+            if (cmd_args.size() < 2) {
+                std::cerr << "Usage: get_config_parameters <slot_id> <filename>\n";
+                return 1;
+            }
+            handlers.get_config_parameters(std::stoi(cmd_args[0]), cmd_args[1]);
+        } else if (command == "monitor") {
+            bool suppress = false;
+            if (!cmd_args.empty() && cmd_args[0] == "--suppress-parameter-updates") {
+                suppress = true;
+            }
+            handlers.monitor(suppress);
+        } else {
+            std::cerr << "Unknown command: " << command << "\n";
+            return 1;
+        }
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << "\n";
+        return 1;
+    }
+
+    return 0;
+}

applications/utils/everest-config-api-cli/src/mqtt_config_service_client.hpp
@@ -1,0 +1,69 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 - 2026 Pionix GmbH and Contributors to EVerest
+#pragma once
+
+#include "config_service_client_ifc.hpp"
+
+#include "everest_api_types/utilities/Topics.hpp"
+#include <everest/io/event/event_fd.hpp>
+#include <everest/io/event/fd_event_handler.hpp>
+#include <everest/io/mqtt/mqtt_client.hpp>
+
+#include <atomic>
+#include <cstdint>
+#include <future>
+#include <memory>
+#include <string>
+#include <thread>
+
+namespace everest::config_cli {
+
+class MqttConfigServiceClient : public ConfigServiceClientIfc {
+public:
+    MqttConfigServiceClient(const std::string& host, std::uint16_t port, const everest::lib::API::Topics& topics,
+                            std::uint32_t reconnect_ms = 5000, bool verbose = false);
+    ~MqttConfigServiceClient() override;
+
+    std::optional<ListSlotIdsResult> list_all_slots() override;
+    std::optional<GetActiveSlotIdResult> get_active_slot() override;
+    std::optional<MarkActiveSlotResult> mark_active_slot(int slot_id) override;
+    std::optional<DeleteSlotResult> delete_slot(int slot_id) override;
+    std::optional<DuplicateSlotResult> duplicate_slot(int slot_id, const std::string& description) override;
+    std::optional<LoadFromYamlResult> load_from_yaml(const std::string& raw_yaml, const std::string& description,
+                                                     std::optional<int> slot_id) override;
+    std::optional<SetDescriptionResult> set_description(int slot_id, const std::string& description) override;
+    std::optional<GetConfigurationResult> get_configuration(int slot_id) override;
+    std::optional<ConfigurationParameterUpdateRequestResult>
+    set_config_parameters(const ConfigurationParameterUpdateRequest& request) override;
+    std::optional<GetConfigurationParameterResult>
+    get_config_parameters(const GetConfigurationParameterRequest& request) override;
+    void subscribe_to_updates(bool suppress_parameter_updates, ActiveSlotCallback active_cb,
+                              ConfigUpdateCallback config_cb) override;
+
+private:
+    std::optional<std::string> perform_rpc_raw(const std::string& operation, const std::string& payload_json);
+
+    template <typename ReqType, typename ResType>
+    std::optional<ResType> perform_rpc(const std::string& operation, const ReqType& req);
+
+    void setup_update_subscriptions();
+
+    everest::lib::io::event::fd_event_handler m_event_handler;
+    everest::lib::io::mqtt::mqtt_client m_client;
+
+    everest::lib::API::Topics m_topics;
+    std::string m_reply_topic_base;
+
+    std::atomic<bool> m_running{true};
+    everest::lib::io::event::event_fd m_cancel_event;
+    std::thread m_event_loop_thread;
+
+    std::promise<void> m_connected_promise;
+    std::shared_future<void> m_connected_future{m_connected_promise.get_future().share()};
+
+    bool m_suppress_parameter_updates{false};
+    ActiveSlotCallback m_active_cb;
+    ConfigUpdateCallback m_config_cb;
+};
+
+} // namespace everest::config_cli