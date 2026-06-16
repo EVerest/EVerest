76ca464edcbd7dc50aeff4c788afe4474789dfc3
Moritz Barsnick <moritz.barsnick@chargebyte.com>
2026-06-16 12:50:29 +0200

    fix(iso15118): change from DCPreCharge to PowerDelivery only after "Finished"
    * feat(iso15118): add fsm/dc_pre_charge test
    
      Taken from branch feat/adding-more-libiso-tests
    
      Co-authored-by: Sebastian Lukas <sebastian.lukas@pionix.de>
      Signed-off-by: Moritz Barsnick <moritz.barsnick@chargebyte.com>
    
    * fix(iso15118): change from DCPreCharge to PowerDelivery only after "Finished"
    
      The D20 state machine was going from DCPreCharge to PowerDelivery without
      first checking that DCPreCharge had EVProcessing = "Finished". It thereby
      accepted premature PowerDelivey from the EV, which should be a sequence
      error.
    
      ISO 15118-20:2022, Table 62, description of DC_PreChargeReq::EVProcessing:
      Parameter that indicates the stop of the DC_PreCharge process by the EV.
      When the EV wants to continue in the Sequence, the EVProcessing is set to
      "Finished", while still running it is set to "Ongoing".
    
      IEC 61851-23-3 (MCS) also specifies this more explicitly.
    
      Note: PyEvJosev and Josev a.k.a iso15118 violates this requirement as well
      (goes to PowerDelivery after DCPreCharge with EVProcessing = "Ongoing"),
      which is why this case succeeded in the SIL.
    
      Fixes https://github.com/EVerest/EVerest/issues/2305
    
      Co-authored-by: Martin Lukas <martin.lukas@chargebyte.com>
      Signed-off-by: Martin Lukas <martin.lukas@chargebyte.com>
      Signed-off-by: Moritz Barsnick <moritz.barsnick@chargebyte.com>
    
    * fix(iso15118): adapt and expand fsm/dc_pre_charge test
    
      Adapt to new handling of EVProcessing.
      Expand with an additional test to cover both Ongoing and Finished.
    
      Signed-off-by: Moritz Barsnick <moritz.barsnick@chargebyte.com>
    
    * fix(iso15118): no longer accept DCPreCharge in PowerDelivery state
    
      Signed-off-by: Moritz Barsnick <moritz.barsnick@chargebyte.com>
    
    Signed-off-by: Moritz Barsnick <moritz.barsnick@chargebyte.com>

--- lib/everest/iso15118/src/iso15118/d20/state/power_delivery.cpp
+++ lib/everest/iso15118/src/iso15118/d20/state/power_delivery.cpp
@@ -105,20 +105,7 @@ Result PowerDelivery::feed(Event ev) {
 
     const auto variant = m_ctx.pull_request();
 
-    if (const auto req = variant->get_if<message_20::DC_PreChargeRequest>()) {
-        const auto res = handle_request(*req, m_ctx.session, present_voltage);
-
-        m_ctx.feedback.dc_pre_charge_target_voltage(dt::from_RationalNumber(req->target_voltage));
-
-        m_ctx.respond(res);
-
-        if (res.response_code >= dt::ResponseCode::FAILED) {
-            m_ctx.session_stopped = true;
-            return {};
-        }
-
-        return {};
-    } else if (const auto req = variant->get_if<message_20::PowerDeliveryRequest>()) {
+    if (const auto req = variant->get_if<message_20::PowerDeliveryRequest>()) {
 
         const auto shutdown_requested = m_ctx.shutdown_requested();
 
@@ -174,7 +161,7 @@ Result PowerDelivery::feed(Event ev) {
         return {};
 
     } else {
-        m_ctx.log("Expected DC_PreChargeReq or PowerDeliveryReq! But code type id: %d", variant->get_type());
+        m_ctx.log("Expected PowerDeliveryReq! But code type id: %d", variant->get_type());
 
         // Sequence Error
         const message_20::Type req_type = variant->get_type();
