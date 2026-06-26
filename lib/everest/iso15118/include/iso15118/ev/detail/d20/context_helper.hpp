b81def644984080ee01f93338b15e4be48c96e03
Martin Litre <mnlitre@gmail.com>
2026-06-26 16:30:56 +0200

    feat(iso15118): EV-side session stack
    Signed-off-by: Martin Litre <mnlitre@gmail.com>

--- lib/everest/iso15118/include/iso15118/ev/detail/d20/context_helper.hpp
+++ lib/everest/iso15118/include/iso15118/ev/detail/d20/context_helper.hpp
@@ -3,17 +3,17 @@
 #pragma once
 
 #include <iso15118/ev/d20/context.hpp>
-#include <iso15118/ev/d20/session.hpp>
+#include <iso15118/ev/d20/session_id.hpp>
 #include <iso15118/message/common_types.hpp>
 
 // Forward declaration
-class Session;
+class SessionId;
 
 namespace iso15118::ev::d20 {
 
-bool validate_and_setup_header(message_20::Header&, const Session&, const decltype(message_20::Header::session_id)&);
+bool validate_and_setup_header(message_20::Header&, const SessionId&, const decltype(message_20::Header::session_id)&);
 
-void setup_header(message_20::Header&, const Session&);
+void setup_header(message_20::Header&, const SessionId&);
 
 bool check_response_code(message_20::datatypes::ResponseCode response_code);
 
