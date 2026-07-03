088196bb91ffdacda8e8705f7563d5f2e5eb503c
Martin Litre <mnlitre@gmail.com>
2026-07-03 12:08:43 +0200

    chore(iso15118): correct test file copyright years
    Signed-off-by: Martin Litre <mnlitre@gmail.com>

--- lib/everest/iso15118/test/iso15118/session/session_teardown.cpp
+++ lib/everest/iso15118/test/iso15118/session/session_teardown.cpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2024 Pionix GmbH and Contributors to EVerest
+// Copyright 2026 Pionix GmbH and Contributors to EVerest
 #include <memory>
 #include <optional>
 
