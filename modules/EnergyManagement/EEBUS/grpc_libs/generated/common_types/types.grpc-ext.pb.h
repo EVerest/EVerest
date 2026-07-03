b93166fc7094f155d95b59792210a19e787bcec7
Martin Litre <mnlitre@gmail.com>
2026-07-03 09:50:51 +0200

    feat(eebus): add EEBUS module with LPC use case (#1561)
    Signed-off-by: Martin Litre <mnlitre@gmail.com>

---
+++ modules/EnergyManagement/EEBUS/grpc_libs/generated/common_types/types.grpc-ext.pb.h
@@ -0,0 +1,10 @@
+#ifndef GENERATED_common_types_types_proto_EXT_H
+#define GENERATED_common_types_types_proto_EXT_H
+
+#include "common_types/types.grpc.pb.h"
+
+namespace common_types {
+
+} // namespace common_types
+
+#endif // GENERATED_common_types_types_proto_EXT_H
---
+++ modules/EnergyManagement/EEBUS/grpc_libs/generated/control_service/messages.grpc-ext.pb.h
@@ -0,0 +1,12 @@
+#ifndef GENERATED_control_service_messages_proto_EXT_H
+#define GENERATED_control_service_messages_proto_EXT_H
+
+#include "control_service/messages.grpc.pb.h"
+#include "control_service/types.grpc-ext.pb.h"
+#include "common_types/types.grpc-ext.pb.h"
+
+namespace control_service {
+
+} // namespace control_service
+
+#endif // GENERATED_control_service_messages_proto_EXT_H
