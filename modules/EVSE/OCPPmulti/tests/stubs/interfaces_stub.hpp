48ef3e94eedf9ce5be02aefeb0e02def4da690b4
Piet Gömpel <pietgoempel@gmail.com>
2026-07-24 08:14:40 +0000

    Removed mutex, not needed anymore
    Signed-off-by: Piet Gömpel <pietgoempel@gmail.com>

--- modules/EVSE/OCPPmulti/tests/stubs/interfaces_stub.hpp
+++ modules/EVSE/OCPPmulti/tests/stubs/interfaces_stub.hpp
@@ -17,7 +17,6 @@
 
 #include <list>
 #include <memory>
-#include <mutex>
 #include <string>
 
 namespace stubs {
@@ -121,8 +120,6 @@ private:
     void publish_fn(const std::string& interface, const std::string& variable, int instance, const json& value);
 
     var_cb_list_t m_subscribe_var_callbacks;
-    // guarded by m_cmd_response_mutex: commands may be invoked from module threads concurrently
-    std::mutex m_cmd_response_mutex;
     std::list<json> m_cmd_response_list;
 
 protected:
