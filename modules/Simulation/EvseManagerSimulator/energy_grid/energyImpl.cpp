c44e5e44cddeb2b894de09b242b2905d10a19b58
Piet Gömpel <pietgoempel@gmail.com>
2026-07-27 11:38:35 +0200

    feat(EvseManagerSimulator): Add new EvseManagerSimulator module (#2466)
    * feat(EvseManagerSimulator): Add new EvseManagerSimulator module that implements a stub for evse_manager.yaml and energy.yaml interface
    
    Signed-off-by: Piet Gömpel <pietgoempel@gmail.com>
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
    
    * Apply suggestions from code review
    
    Co-authored-by: Kai Hermann <kai.uwe.hermann@gmail.com>
    Signed-off-by: Piet Gömpel <pietgoempel@gmail.com>
    
    ---------
    
    Signed-off-by: Piet Gömpel <pietgoempel@gmail.com>
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
    Co-authored-by: Kai Hermann <kai.uwe.hermann@gmail.com>

---
+++ modules/Simulation/EvseManagerSimulator/energy_grid/energyImpl.cpp
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Pionix GmbH and Contributors to EVerest
+
+#include "energyImpl.hpp"
+
+namespace module {
+namespace energy_grid {
+
+void energyImpl::init() {
+}
+
+void energyImpl::ready() {
+}
+
+void energyImpl::handle_enforce_limits(types::energy::EnforcedLimits& value) {
+    // your code for cmd enforce_limits goes here
+}
+
+} // namespace energy_grid
+} // namespace module
