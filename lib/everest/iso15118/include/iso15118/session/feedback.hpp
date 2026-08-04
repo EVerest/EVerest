6e1dcbbc8fdb29633a58ae8e5b455f6f50180a28
Martin Litre <mnlitre@gmail.com>
2026-08-04 19:40:12 +0200

    feat(iso15118): add SAE AC DER feedback variants
    Signed-off-by: Martin Litre <mnlitre@gmail.com>

--- lib/everest/iso15118/include/iso15118/session/feedback.hpp
+++ lib/everest/iso15118/include/iso15118/session/feedback.hpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2023 Pionix GmbH and Contributors to EVerest
+// Copyright 2026 Pionix GmbH and Contributors to EVerest
 #pragma once
 
 #include <cmath>
@@ -16,6 +16,8 @@
 #include <iso15118/message/ac_charge_parameter_discovery.hpp>
 #include <iso15118/message/ac_der_iec_charge_loop.hpp>
 #include <iso15118/message/ac_der_iec_charge_parameter_discovery.hpp>
+#include <iso15118/message/ac_der_sae_charge_loop.hpp>
+#include <iso15118/message/ac_der_sae_charge_parameter_discovery.hpp>
 #include <iso15118/message/dc_charge_loop.hpp>
 #include <iso15118/message/dc_charge_parameter_discovery.hpp>
 #include <iso15118/message/schedule_exchange.hpp>
@@ -108,9 +110,11 @@ using MeterInfoRequested = bool;
 using DcReqControlMode = std::variant<dt::Scheduled_DC_CLReqControlMode, dt::BPT_Scheduled_DC_CLReqControlMode,
                                       dt::Dynamic_DC_CLReqControlMode, dt::BPT_Dynamic_DC_CLReqControlMode>;
 
-using AcReqControlMode = std::variant<dt::Scheduled_AC_CLReqControlMode, dt::BPT_Scheduled_AC_CLReqControlMode,
-                                      dt::DER_Scheduled_AC_CLReqControlMode, dt::Dynamic_AC_CLReqControlMode,
-                                      dt::BPT_Dynamic_AC_CLReqControlMode, dt::DER_Dynamic_AC_CLReqControlMode>;
+using AcReqControlMode =
+    std::variant<dt::Scheduled_AC_CLReqControlMode, dt::BPT_Scheduled_AC_CLReqControlMode,
+                 dt::DER_Scheduled_AC_CLReqControlMode, dt::Dynamic_AC_CLReqControlMode,
+                 dt::BPT_Dynamic_AC_CLReqControlMode, dt::DER_Dynamic_AC_CLReqControlMode,
+                 dt::sae::DER_Scheduled_AC_CLReqControlMode, dt::sae::DER_Dynamic_AC_CLReqControlMode>;
 
 using DcChargeLoopReq = std::variant<DcReqControlMode, dt::DisplayParameters, PresentVoltage, MeterInfoRequested>;
 
@@ -118,12 +122,13 @@ using EvseTransferLimits = std::variant<d20::DcTransferLimits, d20::AcTransferLi
 
 using EvTransferLimits =
     std::variant<dt::DC_CPDReqEnergyTransferMode, dt::BPT_DC_CPDReqEnergyTransferMode, dt::AC_CPDReqEnergyTransferMode,
-                 dt::BPT_AC_CPDReqEnergyTransferMode, dt::DER_AC_CPDReqEnergyTransferMode>;
+                 dt::BPT_AC_CPDReqEnergyTransferMode, dt::DER_AC_CPDReqEnergyTransferMode,
+                 dt::sae::DER_SAE_AC_CPDReqEnergyTransferMode>;
 using EvSEControlMode = std::variant<dt::Dynamic_SEReqControlMode, dt::Scheduled_SEReqControlMode>;
 
 using AcChargeLoopReq = std::variant<AcReqControlMode, dt::DisplayParameters, MeterInfoRequested>;
 using AcLimits = std::variant<dt::AC_CPDReqEnergyTransferMode, dt::BPT_AC_CPDReqEnergyTransferMode,
-                              dt::DER_AC_CPDReqEnergyTransferMode>;
+                              dt::DER_AC_CPDReqEnergyTransferMode, dt::sae::DER_SAE_AC_CPDReqEnergyTransferMode>;
 
 enum class CertificateExchangeAction {
     Install,
