f4f96e2a99977eb52ca5dd290411f7449f170dbd
Martin Litre <mnlitre@gmail.com>
2026-08-05 17:03:33 +0200

    feat(iso15118): answer SAE AC DER charge loop
    Signed-off-by: Martin Litre <mnlitre@gmail.com>

--- lib/everest/iso15118/include/iso15118/d20/state/ac_der_sae_charge_loop.hpp
+++ lib/everest/iso15118/include/iso15118/d20/state/ac_der_sae_charge_loop.hpp
@@ -1,13 +1,23 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2025 Pionix GmbH and Contributors to EVerest
+// Copyright 2026 Pionix GmbH and Contributors to EVerest
 #pragma once
 
 #include "../states.hpp"
+
+#include <cstdint>
+#include <optional>
 
 #include <iso15118/d20/ac_powers.hpp>
 #include <iso15118/d20/dynamic_mode_parameters.hpp>
 
 namespace iso15118::d20::state {
+
+struct SaeChargeLoopLogState {
+    bool target_active_power_function_reported{false};
+    std::optional<std::uint32_t> reported_der_alarm_status;
+    bool reported_enabled_modes_mismatch{false};
+};
+
 struct AC_DER_SAE_ChargeLoop : public StateBase {
     AC_DER_SAE_ChargeLoop(Context& ctx) : StateBase(ctx, StateID::AC_DER_SAE_ChargeLoop) {
     }
@@ -17,13 +27,14 @@ struct AC_DER_SAE_ChargeLoop : public StateBase {
     Result feed(Event) final;
 
 private:
-    std::optional<float> target_frequency; // TODO(SL): Adding updating feature
     bool stop{false};
     bool pause{false};
 
     UpdateDynamicModeParameters dynamic_parameters{};
     AcTargetPower target_powers{};
     AcPresentPower present_powers{};
+
+    SaeChargeLoopLogState log_state{};
 
     bool first_entry_in_charge_loop{true};
 };
