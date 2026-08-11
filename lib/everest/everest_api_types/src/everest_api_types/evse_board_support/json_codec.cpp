b90826bd539ad147dfb309bd8be5f398ecfad4af
Jan Christoph Habig <jan.habig@pionix.de>
2026-08-11 07:42:13 +0200

    BUGFIX: supports_cp_state_E missing
    evse_board_support_API lacks field serialization of supports_cp_state_E in HardwareCapabilities
    
    Signed-off-by: Jan Christoph Habig <jan.habig@pionix.de>

--- lib/everest/everest_api_types/src/everest_api_types/evse_board_support/json_codec.cpp
+++ lib/everest/everest_api_types/src/everest_api_types/evse_board_support/json_codec.cpp
@@ -432,6 +432,7 @@ void to_json(json& j, HardwareCapabilities const& k) noexcept {
         {"max_phase_count_export", k.max_phase_count_export},
         {"min_phase_count_export", k.min_phase_count_export},
         {"supports_changing_phases_during_charging", k.supports_changing_phases_during_charging},
+        {"supports_cp_state_E", k.supports_cp_state_E},
         {"connector_type", k.connector_type},
     };
     if (k.max_plug_temperature_C) {
@@ -449,6 +450,7 @@ void from_json(json const& j, HardwareCapabilities& k) {
     k.max_phase_count_export = j.at("max_phase_count_export");
     k.min_phase_count_export = j.at("min_phase_count_export");
     k.supports_changing_phases_during_charging = j.at("supports_changing_phases_during_charging");
+    k.supports_cp_state_E = j.at("supports_cp_state_E");
     k.connector_type = j.at("connector_type");
 
     if (j.contains("max_plug_temperature_C")) {
