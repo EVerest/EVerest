5b437bca807a06f1689c643a75bb54c42b994711
Martin Litre <mnlitre@gmail.com>
2026-09-25 12:25:43 +0000

    fix(libocpp): local list DisablePostAuthorize (#2891)
    When LocalAuthListCtrlr.DisablePostAuthorize is true and a local-list
    entry is not Accepted, return the stored status instead of sending
    AuthorizeRequest while online, as the AuthCache path already does.
    Offline is unchanged. OCTT 2.0.1 TC_C_58_CS.
    
    Signed-off-by: Martin Litre <mnlitre@gmail.com>

--- lib/everest/ocpp/lib/ocpp/v2/functional_blocks/authorization.cpp
+++ lib/everest/ocpp/lib/ocpp/v2/functional_blocks/authorization.cpp
@@ -313,6 +313,13 @@ ocpp::v2::Authorization::validate_token(const IdToken id_token, const std::optio
                 EVLOG_info << "Found invalid entry in local authorization list but not sending Authorize.req because "
                               "RemoteAuthorization is disabled";
                 response.idTokenInfo.status = AuthorizationStatusEnum::Unknown;
+            } else if (is_online and
+                       this->context.device_model
+                           .get_optional_value<bool>(ControllerComponentVariables::LocalAuthListDisablePostAuthorize)
+                           .value_or(false)) {
+                EVLOG_info << "Found invalid entry in local authorization list: Not sending Authorize.req because "
+                              "LocalAuthListDisablePostAuthorize is enabled";
+                response.idTokenInfo = id_token_info.value();
             } else if (is_online) {
                 // C14.FR.03: If a value found but not valid we shall send an authorize request
                 EVLOG_info << "Found invalid entry in local authorization list: Sending Authorize.req";
